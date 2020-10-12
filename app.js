17a8f61e637aa6c5be322fe639d6f5edbec2b7a9
Manav Arora <manav10799@gmail.com>
2020-10-12 12:08:02 +0530

    Bhuwan1

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ app.use(express.static("public"));
 app.get("/",function(req,res){
     res.render("home");
 })
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
